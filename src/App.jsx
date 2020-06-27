a01b4ea90d7915f29ad5de4e4aac2e0e25a66806
Stephanie Hansen <stephanie@Stephanies-MacBook-Pro.local>
2020-06-27 23:15:13 +0100

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section>
+      <h1>Books App</h1>
+    </section>
+  );
+}
+
+export default App;
