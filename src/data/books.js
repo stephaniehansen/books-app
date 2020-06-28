d50db2332d66a2ccdff94fdcd68f994c427277f5
Stephanie Hansen <stephanie@stephanie.dev>
2020-06-28 01:46:07 +0100

    add sample data

---
+++ src/data/books.js
@@ -0,0 +1,18 @@
+const books = [
+    {
+        id: "001",
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J. K. Rowling",
+        pages: 341,
+        coverImg: "https://www.fictiondb.com/covers/0439064872.jpg"
+    },
+    {
+        id: "002",
+        title: "Tomorrow, When the War Began",
+        author: "John Marsden",
+        pages: 304,
+        coverImg: "https://www.fictiondb.com/covers/044021985X.jpg"
+    },
+];
+
+export default books;
